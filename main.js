45f0bfbba45a7c16765e827a2ceb09b506f423b2
mmd <mmd@Kristapss-MacBook-Pro.local>
2019-11-12 17:14:34 +0100

    Finished categories part 3 - not working properly still, about to rearrange main.js

--- main.js
+++ main.js
@@ -8,17 +8,58 @@ function init() {
     const search = urlParams.get("search");
 
     const id = urlParams.get("id");
+    const category = urlParams.get("category");
 
     if (search) {
-        console.log("this is search result")
+        //console.log("this is search result")
         getSearchData();
-    } else if(id){
+    } else if (id) {
         getSingleEvent();
+    } else if (category) {
+        //category stuff
+
+        getCategoryData(category)
     } else {
-        console.log("not searching")
+        //console.log("not searching")
         getFrontpageData();
     }
 
+    getNavigation()
+
+}
+
+function getNavigation() {
+    fetch("https://janstevica.dk/KEA/2SEM/database/wp-json/wp/v2/categories?per_page=100")
+        .then(res => res.json())
+        .then(data => {
+            //console.log(data)
+            data.forEach(addLink)
+        })
+}
+
+function addLink(oneItem) {
+    //console.log(oneItem.name)
+    //document.querySelector("nav").innerHTML += oneItem.name
+    if (oneItem.parent === 15 && oneItem.count > 0) {
+        const link = document.createElement("a");
+        link.textContent = oneItem.name;
+        link.setAttribute("href", "category.html?category=" + oneItem.id);
+        document.querySelector("nav").appendChild(link);
+    }
+}
+
+function getFrontpageData() {
+    //console.log("getData")
+    fetch("https://janstevica.dk/KEA/2SEM/database/wp-json/wp/v2/event?per_page=100")
+        .then(res => res.json())
+        .then(handleData)
+}
+
+function getCategoryData(catid) {
+    console.log("catid")
+    fetch("https://janstevica.dk/KEA/2SEM/database/wp-json/wp/v2/event?_embed&categories=20" + catid)
+        .then(res => res.json())
+        .then(handleData)
 }
 
 function getSearchData() {
@@ -30,48 +71,39 @@ function getSearchData() {
         .then(handleData)
 }
 
-function getFrontpageData() {
-    console.log("getData")
-    fetch("https://janstevica.dk/KEA/2SEM/database/wp-json/wp/v2/event?per_page=100")
-        .then(res => res.json())
-        .then(handleData)
-}
-
 function getSingleEvent() {
     const urlParams = new URLSearchParams(window.location.search);
     const id = urlParams.get("id");
-    console.log(id)
+    //console.log(id)
 
     fetch("https://janstevica.dk/KEA/2SEM/database/wp-json/wp/v2/event/" + id)
         .then(res => res.json())
         .then(showEvent)
+}
 
-    function showEvent(event) {
-        console.log(event)
+function showEvent(event) {
+    console.log(event)
 
-        const imgPath = event.image.guid;
+    const imgPath = event.image.guid;
 
-        const img = document.querySelector("img.cover");
+    const img = document.querySelector("img.cover");
 
-        img.setAttribute("src", imgPath)
-        img.setAttribute("alt", "Event " + event.title.rendered)
+    img.setAttribute("src", imgPath)
+    img.setAttribute("alt", "Event " + event.title.rendered)
 
-        document.querySelector("h1.eventTitle").textContent = event.title.rendered;
+    document.querySelector("h1.eventTitle").textContent = event.title.rendered;
 
-        document.querySelector("h2.eventDate").textContent = event.event_date;
+    document.querySelector("h2.eventDate").textContent = event.event_date;
 
-        document.querySelector("h2.eventTime").textContent = event.event_time;
+    document.querySelector("h2.eventTime").textContent = event.event_time;
 
-        document.querySelector(".eventPrice").textContent = event.event_price;
+    document.querySelector(".eventPrice").textContent = event.event_price;
 
-        document.querySelector("p.longDescription").textContent = event.long_description;
+    document.querySelector("p.longDescription").textContent = event.long_description;
 
-
-    }
 }
 
 function handleData(myData) {
-    console.log("myData")
     // 1 loop
     myData.forEach(showEvent)
 }
